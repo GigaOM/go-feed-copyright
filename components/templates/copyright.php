3ecfcba190116da3ff3dbc51d782fa874b42480f
Matthew Batchelder <borkweb@gmail.com>
2014-07-01 10:42:54 -0400

    Don't use go_config to get the property name.

--- components/templates/copyright.php
+++ components/templates/copyright.php
@@ -1,5 +1,5 @@
 <p>
-	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> originally published by <a href="<php echo home_url( '/' ); ?>"><?php echo go_config()->get_property(); ?></a>, &copy; copyright <?php echo get_the_date( 'Y' ); ?>. 
+	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> originally published by <a href="<php echo home_url( '/' ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>, &copy; copyright <?php echo get_the_date( 'Y' ); ?>. 
 </p>
 <p>
 	<a href="<?php the_permalink(); ?>">Continue reading&elips;</a>.
