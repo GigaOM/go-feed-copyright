9cfb8525574230b06587322ee433bf3ed4062826
Stephen Page <stephenjpage@gmail.com>
2014-07-31 13:51:52 -0400

    Remove extra period after &hellip

--- components/templates/copyright.php
+++ components/templates/copyright.php
@@ -1,6 +1,6 @@
 <p>
-	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> originally published by <a href="<?php echo home_url( '/' ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>, &copy; copyright <?php echo get_the_date( 'Y' ); ?>. 
+	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> originally published by <a href="<?php echo home_url( '/' ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>, &copy; copyright <?php echo get_the_date( 'Y' ); ?>.
 </p>
 <p>
-	<a href="<?php the_permalink(); ?>">Continue reading&hellip;</a>.
+	<a href="<?php the_permalink(); ?>">Continue reading&hellip;</a>
 </p>
