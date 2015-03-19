2d1dc5caa4f9c561787803c953544058c7435303
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:45:49 -0400

    Update go-feed-copyright.php

--- go-feed-copyright.php
+++ go-feed-copyright.php
@@ -6,6 +6,8 @@
  * Description: Adds copyright lines & UTM codes to feed articles
  * Author: Gigaom Network
  * Author URI: http://gigaom.com
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/components/class-go-feed-copyright.php';
